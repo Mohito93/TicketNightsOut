77da03c86783b9d6b79130605cd1604bc6cfe86e
Mohito93 <mohitkulkarni@hotmail.com>
2014-10-05 00:39:37 +0100

    Added Buy TIcket Controller to StoryBoard

---
+++ ToDoList2/BuyTicketTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BuyTicketTableViewController.h
+//  ToDoList2
+//
+//  Created by Mohit on 5/10/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuyTicketTableViewController : UITableViewController
+
+@end
