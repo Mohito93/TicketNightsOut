925f54677b70b869e610b830cd84c7e3179021a1
Martin Engelcke <martin.engelcke@gmail.com>
2014-10-04 23:42:29 +0100

    martin's commit with the new working form

--- ToDoList2/SellTicketTableViewController.m
+++ ToDoList2/SellTicketTableViewController.m
@@ -44,13 +44,13 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 3;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 4;
+    return 9;
 }
 
 /*
